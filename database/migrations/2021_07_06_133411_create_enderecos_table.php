8b802f25bbd5767961f34ebe19529248ac60f923
Vinicius <solutiontecforyou@gmail.com>
2021-07-06 12:30:24 -0300

    create migration "create_enderecos_table"

---
+++ database/migrations/2021_07_06_133411_create_enderecos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnderecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cliente_id');
+            $table->text('cep');
+            $table->text('logradouro');
+            $table->text('numero');
+            $table->text('UF');
+            $table->text('complemento')->nullable();
+            $table->text('bairro')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}
