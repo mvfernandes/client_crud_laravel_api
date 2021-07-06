cc3513fc006175b6dc197f1c5e0a44c2c0b71592
Vinicius <solutiontecforyou@gmail.com>
2021-07-06 12:33:15 -0300

    setup api routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/clientes', [ClienteController::class, 'index']);
+Route::post('/cliente', [ClienteController::class, 'store']);
+Route::get('/cliente/{id}', [ClienteController::class, 'show']);
+Route::put('/cliente/{id}', [ClienteController::class, 'update']);
+Route::delete('/cliente/{id}', [ClienteController::class, 'destroy']);
+Route::post('/endereco/{id}', [ClienteController::class, 'storeAddress']);
+Route::put('/endereco/{idUser}/{idAddress}', [ClienteController::class, 'updateAddress']);
+Route::delete('/endereco/{idUser}/{idAddress}', [ClienteController::class, 'destroyAddress']);
