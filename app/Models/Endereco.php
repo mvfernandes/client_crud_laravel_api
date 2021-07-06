26405f41e572d3e5ce5dc21331937175498ddf81
Vinicius <solutiontecforyou@gmail.com>
2021-07-06 12:31:24 -0300

    create model "Endereco"

---
+++ app/Models/Endereco.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Endereco extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'cep',
+        'logradouro',
+        'numero',
+        'UF',
+        'bairro',
+        'complemento',
+    ];
+}
